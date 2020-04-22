2c72c50d916f3591b583c44bc1749d6bb2d65b58
Thomas Venturini <thomas@du.digital>
2020-04-22 17:19:36 +0200

    move provider seeder to stripe package

--- database/seeds/DemoDataSeeder.php
+++ database/seeds/DemoDataSeeder.php
@@ -4,6 +4,7 @@ namespace Marqant\MarqantPay\Seeds;
 
 use Illuminate\Database\Seeder;
 use Marqant\MarqantPaySubscriptions\Seeds\PlansTableSeeder;
+use Marqant\MarqantPayStripe\Seeds\StripeProvidersTableSeeder;
 use Marqant\MarqantPaySubscriptions\Seeds\PlanProviderTableSeeder;
 
 class DemoDataSeeder extends Seeder
@@ -15,8 +16,12 @@ class DemoDataSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ProvidersTableSeeder::class);
+        // run seeders for marqant/marqant-pay-stripe package if available
+        if (class_exists(StripeProvidersTableSeeder::class)) {
+            $this->call(StripeProvidersTableSeeder::class);
+        }
 
+        // run seeders for marqant/marqant-pay-subscriptions package if available
         if (class_exists(PlansTableSeeder::class) && class_exists(PlanProviderTableSeeder::class)) {
             $this->call(PlansTableSeeder::class);
             $this->call(PlanProviderTableSeeder::class);
