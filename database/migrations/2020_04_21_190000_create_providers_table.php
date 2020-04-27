afec5cfcd7078000475fe284d793d14200d01e1b
Thomas Venturini <thomas@du.digital>
2020-04-27 16:49:02 +0200

    bugfix in migrations

--- database/migrations/2020_04_21_190000_create_providers_table.php
+++ database/migrations/2020_04_21_190000_create_providers_table.php
@@ -14,7 +14,7 @@ class CreateProvidersTable extends Migration
     public function up()
     {
         Schema::create('providers', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
 
             $table->string('name');
             $table->string('slug');
--- database/migrations/2020_04_21_190001_create_payments_table.php
+++ database/migrations/2020_04_21_190001_create_payments_table.php
@@ -14,7 +14,7 @@ class CreatePaymentsTable extends Migration
     public function up()
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
 
             $table->string('provider');
             $table->integer('amount');
